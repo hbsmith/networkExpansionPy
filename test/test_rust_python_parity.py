a082208e61b4259856f5cf95e1665bb879487c22
hbsmith <hbs@asu.edu>
2025-12-08 10:21:19 +0900

    remove warning call since it's handled by separate file

--- test/test_rust_python_parity.py
+++ test/test_rust_python_parity.py
@@ -1,7 +1,3 @@
-# import warnings
-# from scipy.sparse import SparseEfficiencyWarning
-# warnings.filterwarnings('ignore', category=SparseEfficiencyWarning)
-
 import unittest
 import numpy as np
 from scipy.sparse import csr_matrix
